cfc8a9a234854a41f6281fcc86216e70debaf746
Emmanuel Bernard <emmanuel.bernard@e-teach.ch>
2019-12-04 18:05:12 +0100

    Rename html folder in views

--- js/PresentationCreate.js
+++ js/PresentationCreate.js
@@ -1,13 +1,13 @@
 function createPresentation(templateId, name, folderId) {
   var copyFile = {
-    title: formatDate() + ' ' + name,
-    parents: [{id: folderId}]
+    title: formatDate() + " " + name,
+    parents: [{ id: folderId }]
   };
-  
+
   copyFile = Drive.Files.copy(copyFile, templateId);
 
   var presentationId = copyFile.id;
 
-  Logger.log('Created new presentation id: ' + presentationId);
+  Logger.log("Created new presentation id: " + presentationId);
   return copyFile;
 }
