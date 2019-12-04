7b5dc33ffd4bf0f26c8c3c1564f1c945e5068975
Emmanuel Bernard <emmanuel.bernard@e-teach.ch>
2019-12-04 17:03:30 +0100

    Clean unused files

--- Code.js
+++ Code.js
@@ -2,88 +2,91 @@
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
   // Or DocumentApp or FormApp.
-  ui.createMenu('Simple Templating')
-      .addItem('Open Sidebar', 'showSidebar')
-      .addToUi();
+  ui.createMenu("Simple Templating")
+    .addItem("Open Sidebar", "showSidebar")
+    .addToUi();
 }
 
 // On click on the start button, from menu, show sidebar
 function showSidebar() {
-  sidebar_html = HtmlService.createHtmlOutputFromFile('Page')
-      .setTitle('Simple Templating')
-      .setWidth(300);
+  sidebar_html = HtmlService.createHtmlOutputFromFile("Page")
+    .setTitle("Simple Templating")
+    .setWidth(300);
   SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
-      .showSidebar(sidebar_html);
+    .showSidebar(sidebar_html);
 }
 
 // Called from page : open the picker in "template" mode
-function selectTemplate(){
+function selectTemplate() {
   CacheService.getDocumentCache().put("picker_mode", "template", 1500000000);
   showPicker();
 }
 
 // Called from page : open the picker in "folder" mode
-function selectFolder(){
+function selectFolder() {
   CacheService.getDocumentCache().put("picker_mode", "folder", 1500000000);
   showPicker();
 }
 
-function saveName(name){
+function saveName(name) {
   CacheService.getDocumentCache().put("name", name, 1500000000);
 }
-                                    
-function saveMode(modeCode){
+
+function saveMode(modeCode) {
   CacheService.getDocumentCache().put("mode", modeCode, 1500000000);
 }
 
 // Show file picker
 function showPicker() {
-  var html = HtmlService.createHtmlOutputFromFile('Picker.html')
-      .setWidth(600)
-      .setHeight(425)
-      .setSandboxMode(HtmlService.SandboxMode.IFRAME);
-   SpreadsheetApp.getUi().showModalDialog(html, 'Select ' + CacheService.getDocumentCache().get("picker_mode"));
+  var html = HtmlService.createHtmlOutputFromFile("Picker.html")
+    .setWidth(600)
+    .setHeight(425)
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
+  SpreadsheetApp.getUi().showModalDialog(
+    html,
+    "Select " + CacheService.getDocumentCache().get("picker_mode")
+  );
 }
 
 // Call pack on picker is picked, save the file in cache for reuse in checkPickerUpdate
-function onPickerPicked(file){
-  if(CacheService.getDocumentCache().get("picker_mode") == 'template'){
-     CacheService.getDocumentCache().put("template", file, 1500000000);
-  } else  if(CacheService.getDocumentCache().get("picker_mode") == 'folder'){
-     CacheService.getDocumentCache().put("folder", file, 1500000000);
+function onPickerPicked(file) {
+  if (CacheService.getDocumentCache().get("picker_mode") == "template") {
+    CacheService.getDocumentCache().put("template", file, 1500000000);
+  } else if (CacheService.getDocumentCache().get("picker_mode") == "folder") {
+    CacheService.getDocumentCache().put("folder", file, 1500000000);
   }
-  
-   // CacheService.getDocumentCache().remove("picker_mode");
+
+  // CacheService.getDocumentCache().remove("picker_mode");
 }
 
 // Called from page, check if the picker has something to return
-function getFiles(){
+function getFiles() {
   var template = CacheService.getDocumentCache().get("template");
   var folder = CacheService.getDocumentCache().get("folder");
-  
+
   var data = {
-      template:  template,
-      folder : folder
-    };
-    
-    return data;
+    template: template,
+    folder: folder
+  };
+
+  return data;
 }
 
 // Called from page, check if the picker has something to return
-function getData(){
+function getData() {
   var template = CacheService.getDocumentCache().get("template");
-  var folder = CacheService.getDocumentCache().get("folder");    
+  var folder = CacheService.getDocumentCache().get("folder");
   var name = CacheService.getDocumentCache().get("name");
-  var mode = CacheService.getDocumentCache().get("mode"); 
-  
+  var mode = CacheService.getDocumentCache().get("mode");
+
   var data = {
-      template:  template,
-      folder : folder,
-      name: name,
-      mode: mode
-    };
-    
-    return data;
+    template: template,
+    folder: folder,
+    name: name,
+    mode: mode
+  };
+
+  return data;
 }
 
 // Return token (for filepicker)
@@ -91,40 +94,36 @@ function getOAuthToken() {
   var token = ScriptApp.getOAuthToken();
   DriveApp.getRootFolder();
   var payLoad = {
-    token : token,
+    token: token,
     mode: CacheService.getDocumentCache().get("picker_mode")
   };
   return payLoad;
 }
 
 function generate() {
-  var templateId = JSON.parse(CacheService.getDocumentCache().get("template")).id; 
-  var folderId =  JSON.parse(CacheService.getDocumentCache().get("folder")).id;
+  var templateId = JSON.parse(CacheService.getDocumentCache().get("template"))
+    .id;
+  var folderId = JSON.parse(CacheService.getDocumentCache().get("folder")).id;
   var dataBaseId = SpreadsheetApp.getActive().getId();
   var name = CacheService.getDocumentCache().get("name");
   var mode = CacheService.getDocumentCache().get("mode");
-  
+
   generate_cards(templateId, folderId, dataBaseId, name, mode);
 }
 
-
-
 function generate_cards(templateId, folderId, dataBaseId, name, mode) {
-  
   /* Déroulement */
-  
-  // 1 : récupération de la liste des cartes dans la sheet
- var cards = getCards(dataBaseId);
 
+  // 1 : récupération de la liste des cartes dans la sheet
+  var cards = getCards(dataBaseId);
 
   // 2 : on créé le fichier à partir du template souhaité, avec le nom paramétré
- var presentation = createPresentation(templateId, name, folderId);
-  
+  var presentation = createPresentation(templateId, name, folderId);
+
   // 3 : on construit la liste des cartes
   createCards(cards, presentation.id, mode);
-  
+
   // var presentation = SlidesApp.openById(presentationId);
 
   return presentation.id;
 }
-
