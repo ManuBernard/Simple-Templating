cb683f2d9448abe2bb3dc6bfcea5a932dcbb95c3
Emmanuel Bernard <emmanuel.bernard@e-teach.ch>
2019-12-04 17:52:08 +0100

    Re-add missing file getcards

--- GenerateManual.js
+++ GenerateManual.js
@@ -2,7 +2,7 @@ function manuallyGenerate() {
   var templateId = "1-I5cKguvqIXyl7fwe9UvOWrlnbphurHDvX-w6QI9KMo";
   var folderId = "1FsimT39FSr_Vmcfj-nzjACL5u9b_8F9c";
   var dataBaseId = "1Gd5KUoSqw9sRCEKhkafZl0akeyf0jpM_vxV_KHhGmHw";
-  var name = "test";
+  var name = "Manually generated";
   console.log("NEW MANUEL GENERZATION");
   generate_cards(templateId, folderId, dataBaseId, name);
 }
