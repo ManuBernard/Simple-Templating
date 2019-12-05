b95aff8d6c493d63b04e2f95cfbca336cb6bc235
Emmanuel Bernard <emmanuel.bernard@e-teach.ch>
2019-12-05 08:26:19 +0100

    Change Template ID and Folder ID for Manuel debug

--- GenerateManual.js
+++ GenerateManual.js
@@ -1,10 +1,12 @@
 // Manually generate the cards
 
 function manuallyGenerate() {
-  var templateId = "1-I5cKguvqIXyl7fwe9UvOWrlnbphurHDvX-w6QI9KMo";
-  var folderId = "1FsimT39FSr_Vmcfj-nzjACL5u9b_8F9c";
+  var templateId = "19q7BnfLZQ4X7d5y6WET7_BT1zlP_j4AF56zBebfaq58";
+  var folderId = "1J8uE1iqAZnLTaW0AH1SvW_GjMj4hB7eA";
   var dataBaseId = "1Gd5KUoSqw9sRCEKhkafZl0akeyf0jpM_vxV_KHhGmHw";
   var name = "Manually generated";
+  
   console.log("NEW MANUEL GENERZATION");
+  
   generate_cards(templateId, folderId, dataBaseId, name);
 }
