6c0ce9965be09e38bfe0496649c6fccd2f364d62
Emmanuel Bernard <emm.bernard@gmail.com>
2020-04-27 13:04:50 +0200

    FTP push config

--- Website/SimpleTemplatingWebsite/src/components/feedback.vue
+++ Website/SimpleTemplatingWebsite/src/components/feedback.vue
@@ -1,16 +1,26 @@
+
 <template>
-  <v-container style="display: none">
-    <v-card dark fluid color="secondary">
+  <v-container>
+    <v-card
+      dark
+      fluid
+      color="secondary"
+    >
       <v-card-text>
         <v-row>
           <v-col class="text-left">
-            <span class="subtitle-1"
-              >During Beta, all features are free to use, your feedback is
-              important!</span
-            >
+            <span class="subtitle-1">During Beta, all features are free to use, your feedback is
+              important!</span>
           </v-col>
           <v-col class="text-center">
-            <v-btn color="white" light outlined block>Give your feedback</v-btn>
+            <v-btn
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfotkZ_a2HS_64IWPsgp8MDxGt009UVnoLe3zmudmT5TIpNug/viewform?usp=sf_link"
+              target="_blank"
+              color="white"
+              light
+              outlined
+              block
+            >Give your feedback</v-btn>
           </v-col>
         </v-row>
       </v-card-text>
